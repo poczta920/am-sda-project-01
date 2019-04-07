17d93cb41036978e0e314dd81a10517cb6ff704c
Adam Malina <poczta920@gmail.com>
2019-04-07 14:33:04 +0200

    Create entity

--- src/main/java/am/sda/domain/Address.java
+++ src/main/java/am/sda/domain/Address.java
@@ -1,4 +1,29 @@
 package am.sda.domain;
 
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+@Data
+@NoArgsConstructor
+@Entity
+@Table(name = "address")
 public class Address {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    int idAddress;
+
+    @Column(name = "city", nullable = false)
+    String city;
+
+    @Column(name = "streat")
+    String streat;
+
+    @Column(name = "zipcode")
+    String zipCode;
+
+    @ManyToOne(cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+    @JoinColumn(name = "id_person")
+    Person person;
+
 }
--- src/main/java/am/sda/domain/Person.java
+++ src/main/java/am/sda/domain/Person.java
@@ -1,4 +1,38 @@
 package am.sda.domain;
 
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+
+@Entity
+@Table(name = "person")
 public class Person {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_person")
+    int idPerson;
+
+    @Column(name = "first_name", nullable = false, length = 50)
+    String firstName;
+
+    @Column(name = "last_name", nullable = false, length = 50)
+    String lastName;
+
+    @Column(name = "birth_day")
+    LocalDate birthDay;
+
+    @Column(name = "email", length = 30)
+    String eamil;
+
+    @Column(name = "phone")
+    String phone;
+
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "person")
+    List<Address> address;
 }
